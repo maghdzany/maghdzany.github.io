1bb606af556bdb6f0b55f978c0ec5e0021a220e0
maghdzany <maghdzany@gmail.com>
2023-09-14 22:14:55 +0700

    first commit

---
+++ dist/script.js
@@ -0,0 +1,35 @@
+const menuToggle = document.getElementById('menuToggle');
+const closeSidebar = document.getElementById('closeSidebar');
+const sidebar = document.getElementById('sidebar');
+const sidebarDimming = document.getElementById('sidebarDimming');
+const navbar = document.getElementById('navbar');
+        
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.remove('w-0', 'translate-x-full');
+    sidebar.classList.add('w-64', 'translate-x-0');
+    sidebarDimming.classList.remove('hidden');
+});
+
+closeSidebar.addEventListener('click', () => {
+    sidebar.classList.remove('w-64', 'translate-x-0');
+    sidebar.classList.add('w-0', 'translate-x-full');
+    sidebarDimming.classList.add('hidden');
+});
+
+sidebarDimming.addEventListener('click', () => {
+    sidebar.classList.remove('w-64', 'translate-x-0');
+    sidebar.classList.add('w-0', 'translate-x-full');
+    sidebarDimming.classList.add('hidden');
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        // Saat scroll down, munculkan navbar dengan efek fade dan scale
+        navbar.classList.remove('scale-y-0', 'opacity-0');
+        navbar.classList.add('scale-y-100', 'opacity-100');
+    } else {
+        // Saat di paling atas, sembunyikan navbar kembali
+        navbar.classList.remove('scale-y-100', 'opacity-100');
+        navbar.classList.add('scale-y-0', 'opacity-0');
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        bgdark: '#0b0e2f',
+        bgbiru: '#64428e',
+        aksen1: '#44a1fe',
+        aksen2: '#14c2f7',
+        biru: '#3f5bfa',
+        primary: '#ffffff',
+        secondary: '#777777',
+        textdark: '#0f172a',
+        card1: '#64428e',
+        card2: '#2b417a',
+        mail1: '#f62a4b',
+        mail2: '#ff5800',
+        wa1: '#b5cf47',
+        wa2: '#e0de20',
+        fb1: '#5a71be',
+        fb2: '#2fbee1',
+        ig1: '#d42374',
+        ig2: '#f62a4b',
+        tw1: '#2fbee1',
+        tw2: '#17eef5',
+        dc1: '#7749ac',
+        dc2: '#5a71be',
+
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
